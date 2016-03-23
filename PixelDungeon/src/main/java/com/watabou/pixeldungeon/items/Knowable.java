22b7d66e9027ee7a48a070d0dc7955f8c07ac793
wariotx <leopoldonegro@gmail.com>
2016-03-23 21:28:40 +0100

    Knowable interface for badges

---
+++ PixelDungeon/src/main/java/com/watabou/pixeldungeon/items/Knowable.java
@@ -0,0 +1,9 @@
+package com.watabou.pixeldungeon.items;
+
+/**
+ * Created by Leopoldo on 23/03/2016.
+ */
+public interface Knowable {
+	boolean isKnown();
+	void setKnown();
+}
---
+++ PixelDungeon/src/main/java/com/watabou/pixeldungeon/items/KnowableItem.java
@@ -0,0 +1,14 @@
+package com.watabou.pixeldungeon.items;
+
+/**
+ * Created by Leopoldo on 23/03/2016.
+ */
+public abstract class KnowableItem extends Item implements Knowable{
+
+	public abstract boolean isKnown();
+	public abstract void setKnown();
+	@Override
+	public boolean isIdentified() {
+		return isKnown();
+	}
+}
